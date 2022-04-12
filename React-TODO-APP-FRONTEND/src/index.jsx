6522bf493a7b50429963b5ea1ff6cbd9a33fdd80
jpvieirah <jp201101@gmail.com>
2022-04-12 12:39:10 -0300

    Aplicando Middleware (Redux-Thunk)

--- React-TODO-APP-FRONTEND/src/index.jsx
+++ React-TODO-APP-FRONTEND/src/index.jsx
@@ -6,9 +6,10 @@ import { Provider } from 'react-redux'
 import reducers from './main/reducers'
 import promise from "redux-promise";
 import multi from 'redux-multi'
+import thunk from "redux-thunk";
 
 
-const store = applyMiddleware(multi, promise)(createStore)(reducers)
+const store = applyMiddleware(thunk, multi, promise)(createStore)(reducers)
 ReactDOM.render(
     <Provider store={store}>
 <App/>
